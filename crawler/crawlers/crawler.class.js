037ffa4768f4502a0b3576b0cb2d2b80382fab45
Richard Onyon <onyon@Richards-MacBook-Pro.local>
2022-08-09 01:40:16 -0700

    Fallback for releasedate.

--- crawler/crawlers/crawler.class.js
+++ crawler/crawlers/crawler.class.js
@@ -105,7 +105,7 @@ export default class Crawler {
       Title: Joi.string().min(1).max(255).required(),
       Description: Joi.any().required(),
       Year: Joi.number().integer().min(1900).max(2024).required(),
-      ReleaseDate: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
+      ReleaseDate: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required().default(`${input.Year}-01-01`),
       Link: Joi.string().uri({ scheme: ["http","https"]}).required(),
       RetrieveImage: Joi.any().required(),
       Slug: Joi.string().default(this.createSlug(`s${input.Season}e${input.Episode}-${input.Title}`)),
